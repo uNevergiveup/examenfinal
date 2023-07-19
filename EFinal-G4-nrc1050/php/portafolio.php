62c7b1d8631f334c6422f896dc087e1e6e2e1567
Andrwes Estrada Castillo <1977822@tcs.com>
2023-07-18 23:52:32 -0500

    add files

--- EFinal-G4-nrc1050/php/portafolio.php
+++ EFinal-G4-nrc1050/php/portafolio.php
@@ -9,9 +9,9 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         
     <!-- Enlazar el archivo HTML con una hoja de estilos externa CSS -->
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="../css/style.css">
            
-        <link rel="shortcut icon" href="imagenes/1.Icono.png" type="image/x-icon">
+        <link rel="shortcut icon" href="../img/1.Icono.png" type="image/x-icon">
     
     <!-- Enlazar Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -60,8 +60,8 @@
 <header id="cabecera" class="espaciado">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="logo">
-            <a class="navbar-brand" href="index.html">
-                <img src="imagenes/1.Logo.png" width="200" height="120" alt="">
+            <a class="navbar-brand" href="index.php">
+                <img src="../img/1.Logo.png" width="200" height="120" alt="">
             </a>   
         </div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -70,20 +70,20 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul  class="nav justify-content-end">
             <li class="nav-item">
-              <a class="nav-link active" href="index.html">Inicio<span class="sr-only">(current)</span></a>
+              <a class="nav-link active" href="index.php">Inicio<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link " href="nosotros.html">Nosotros</a>
+              <a class="nav-link " href="nosotros.php">Nosotros</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="servicios.html">Servicios</a>
+                <a class="nav-link" href="servicios.php">Servicios</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="portafolio.html">Portafolio</a>
+                <a class="nav-link" href="portafolio.php">Portafolio</a>
             </li>
     
             <li class="nav-item">
-                <a class="nav-link" href="contacto.html">Contacto</a>
+                <a class="nav-link" href="contacto.php">Contacto</a>
             </li>
           </ul>
         </div>
@@ -96,7 +96,7 @@
             <p>
                 <i class="fa-solid fa-location-dot"></i>
     
-                <a href="index.html">Inicio</a>  |  Portafolio
+                <a href="index.php">Inicio</a>  |  Portafolio
             </p>
         </div> <!-- contenedor -->
     </section> <!-- barra-portafolio -->
@@ -105,15 +105,15 @@
         <div class="contenedor">
             <nav class="pie barra">
                 <div class="pie-logo">
-                    <img src="imagenes/1.Logo.png" alt="">
+                    <img src="../img/1.Logo.png" alt="">
                 </div>
                 <ul class="pie-opciones">
                     <div class="pie-items">
-                        <li><a href="index.html">Inicio</a></li>
-                        <li><a href="nosotros.html">Nosotros</a></li>
-                        <li><a href="servicios.html">Servicios</a></li>
-                        <li><a href="portafolio.html">Portafolio</a></li>
-                        <li><a href="contacto.html">Contacto</a></li>
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="nosotros.php">Nosotros</a></li>
+                        <li><a href="servicios.php">Servicios</a></li>
+                        <li><a href="portafolio.php">Portafolio</a></li>
+                        <li><a href="contacto.php">Contacto</a></li>
                     </div>
                 </ul>
             </nav>
