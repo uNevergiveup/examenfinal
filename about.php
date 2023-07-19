5e21e566a2bba34f01246611e658c1c13c4af66f
Andrwes Estrada Castillo <1977822@tcs.com>
2023-07-18 22:54:01 -0500

    Add new files

--- about.php
+++ about.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,13 +11,31 @@
           integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
           crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
 
 </head>
 <body>
-
-
-
-
+<div class="container">
+    <div class="about-text">
+        <p>Acerca de la empresa</p>
+        <h1>Proveedor de alimentos orgánicos y saludables Agricultura</h1>
+        <p>
+            Organic - Ecotienda se dedica a la agricultura y se enorgullece de ofrecer
+            productos frescos y nutritivos a sus clientes. Comprometidos con la producción sostenible, cultivan una
+            amplia
+            variedad de alimentos utilizando prácticas agrícolas respetuosas con el medio ambiente. Desde frutas y
+            verduras
+            hasta granos y hierbas, cada producto es cultivado cuidadosamente sin el uso de pesticidas o fertilizantes
+            químicos. Además, se aseguran de mantener altos estándares de calidad y trazabilidad en todas las etapas de
+            producción, desde la siembra hasta la entrega. Con un enfoque en la salud y el bienestar, la empresa se
+            esfuerza
+            por brindar opciones alimenticias que promuevan un estilo de vida saludable y contribuyan al cuidado del
+            planeta.</p>
+    </div>
+    <div class="about-img">
+        <img src="about-img.jpg" alt="">
+    </div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
         integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
         crossorigin="anonymous"></script>
